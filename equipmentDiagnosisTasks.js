03b9293b45e2b69d7595cf4eff0332d8e959a0e0
Aarav Desai <aaravdesai@berkeley.edu>
2024-04-28 16:00:49 -0700

    equipment diagnosis intial progress

---
+++ equipmentDiagnosisTasks.js
@@ -0,0 +1,267 @@
+// diagnosisTasks.js
+
+function onDiagnosisMenuBeginSubtask1() {
+    return {
+      function: "on_diagnosis_menu_begin_subtask_1_HMD",
+      parameter: {
+        display_string: "Cable Repair procedure received! Please follow the given steps to repair the cable. Let's start 'shut down comm tower'. Tell me 'finished' when you are done."
+      }
+    };
+  }
+
+function onDiagnosisMenuDoSubtask1a() {
+    return {
+      function: "on_diagnosis_menu_do_subtask_1a_HMD",
+      parameter: {
+        display_string: "Select power button on screen. Say 'Ursa, Next Step' to proceed."
+      }
+    };
+  }
+  
+  function onDiagnosisMenuDoSubtask1b() {
+    return {
+      function: "on_diagnosis_menu_do_subtask_1b_HMD",
+      parameter: {
+        display_string: "Select shut down option. Say 'Ursa, Next Step' to proceed."
+      }
+    };
+  }
+  
+  function onDiagnosisMenuDoSubtask1c() {
+    return {
+      function: "on_diagnosis_menu_do_subtask_1c_HMD",
+        parameter: {
+          display_string: "Replay system shutdown complete. Say 'Ursa, Next Step' to proceed."
+        }
+    };
+  }
+
+  function onDiagnosisMenuBeginSubtask2() {
+    return {
+      function: "on_diagnosis_menu_begin_subtask_2_HMD",
+      parameter: {
+        display_string: "Great work, you've completed step 1. Now, let's begin 'Power down MMRTG'. Tell me 'finished' when you are done."
+      }
+    };
+  }
+  
+  function onDiagnosisMenuDoSubtask2a() {
+    return {
+      function: "on_diagnosis_menu_do_subtask_2a_HMD",
+        parameter: {
+          display_string: "Set power switch to off position. Say 'Ursa, Next Step' to proceed."
+        }
+    };
+  }
+  
+  function onDiagnosisMenuDoSubtask2b() {
+    return {
+      function: "on_diagnosis_menu_do_subtask_2b_HMD",
+      parameter: {
+        display_string: "Relay power down to MCC. Say 'Ursa, Next Step' to proceed."
+      }
+    };
+  }
+  
+  function onDiagnosisMenuDoSubtask2c() {
+    return {
+      function: "on_diagnosis_menu_do_subtask_2c_HMD",
+      parameter: {
+        display_string: "Return to comm tower for cable swap task. Say 'Ursa, Next Step' to proceed."
+      }
+    };
+  }
+
+  function onDiagnosisMenuBeginSubtask3() {
+    return {
+      function: "on_diagnosis_menu_begin_subtask_3_HMD",
+      parameter: {
+        display_string: "Great work, you've completed step 2. Now, let's begin 'Cable Swap'. Tell me 'finished' when you are done."
+      }
+    };
+  }
+  
+  function onDiagnosisMenuDoSubtask3a() {
+    return {
+      function: "on_diagnosis_menu_do_subtask_3a_HMD",
+      parameter: {
+        display_string: "Retrieve functional cable (EV1). Say 'Ursa, Next Step' to proceed."
+      }
+    };
+  }
+  
+  function onDiagnosisMenuDoSubtask3b() {
+    return {
+      function: "on_diagnosis_menu_do_subtask_3b_HMD",
+      parameter: {
+        display_string: "Take one end of the cable to the MMRTG site. Relay when arrived (EV2). Say 'Ursa, Next Step' to proceed."
+      }
+    };
+  }
+  
+  
+  function onDiagnosisMenuDoSubtask3c() {
+    return {
+      function: "on_diagnosis_menu_do_subtask_3c_HMD",
+      parameter: {
+        display_string: "Disconnect damaged cable from comm tower. Relay when complete (EV1). Say 'Ursa, Next Step' to proceed."
+      }
+    };
+  }
+  
+  function onDiagnosisMenuDoSubtask3d() {
+    return {
+      function: "on_diagnosis_menu_do_subtask_3d_HMD",
+      parameter: {
+        display_string: "Disconnect damaged cable from comm tower. Relay when complete (EV2). Say 'Ursa, Next Step' to proceed."
+      }
+    };
+  }
+
+  function onDiagnosisMenuDoSubtask3e() {
+    return {
+      function: "on_diagnosis_menu_do_subtask_3e_HMD",
+      parameter: {
+        display_string: "Connect functional cable to comm tower, relay when complete (EV1). Say 'Ursa, Next Step' to proceed."
+      }
+    };
+  }
+
+  function onDiagnosisMenuDoSubtask3f() {
+    return {
+      function: "on_diagnosis_menu_do_subtask_3f_HMD",
+      parameter: {
+        display_string: "Connect functional cable to MMRTG, relay when complete (EV2). Say 'Ursa, Next Step' to proceed."
+      }
+    };
+  }
+
+  function onDiagnosisMenuDoSubtask3g() {
+    return {
+      function: "on_diagnosis_menu_do_subtask_3g_HMD",
+      parameter: {
+        display_string: "Relay cable swap complete to MCC (EV1). Say 'Ursa, Next Step' to proceed."
+      }
+    };
+  }
+
+  function onDiagnosisMenuBeginSubtask4() {
+    return {
+      function: "on_diagnosis_menu_begin_subtask_4_HMD",
+      parameter: {
+        display_string: "Great work, you've completed step 3. Now, let's begin 'Restore Power'. Tell me 'finished' when you are done."
+      }
+    };
+  }
+  
+  function onDiagnosisMenuDoSubtask4a() {
+    return {
+      function: "on_diagnosis_menu_do_subtask_4a_HMD",
+      parameter: {
+        display_string: "Set MMRTG power switch to 'on' position (EV2). Say 'Ursa, Next Step' to proceed."
+      }
+    };
+  }
+  
+  function onDiagnosisMenuDoSubtask4b() {
+    return {
+      function: "on_diagnosis_menu_do_subtask_4b_HMD",
+      parameter: {
+        display_string: "Power on comm tower by pressing the power button (EV1). Say 'Ursa, Next Step' to proceed."
+      }
+    };
+  }
+
+  function onDiagnosisMenuDoSubtask4c() {
+    return {
+      function: "on_diagnosis_menu_do_subtask_4c_HMD",
+      parameter: {
+        display_string: "Wait for system start up (EV1). Say 'Ursa, Next Step' to proceed."
+      }
+    };
+  }
+
+  function onDiagnosisMenuDoSubtask4d() {
+    return {
+      function: "on_diagnosis_menu_do_subtask_4d_HMD",
+      parameter: {
+        display_string: "Relay successful system start (EV1). Say 'Ursa, Next Step' to proceed."
+      }
+    };
+  }
+
+  function onDiagnosisMenuBeginSubtask5() {
+    return {
+      function: "on_diagnosis_menu_begin_subtask_5_HMD",
+      parameter: {
+        display_string: "Great work, you've completed step 4. Now, let's begin 'Verify Successful Repair (Both EVs)'. Tell me 'finished' when you are done."
+      }
+    };
+  }
+
+  function onDiagnosisMenuDoSubtask5a() {
+    return {
+      function: "on_diagnosis_menu_do_subtask_5a_HMD",
+      parameter: {
+        display_string: "Verify channel is live on comm tower display. Say 'Ursa, Next Step' to proceed."
+      }
+    };
+  }
+  
+  function onDiagnosisMenuDoSubtask5b() {
+    return {
+      function: "on_diagnosis_menu_do_subtask_5b_HMD",
+      parameter: {
+        display_string: "Test if comm channel is functional, following the channel switch protocol: 1) MCC relays channel switch start. 2) EV will swap to repaired channel. 3) If no comms are received within 10 seconds, EV will return to working channel. 4) Otherwise proceed. Say 'Ursa, Next Step' to proceed."
+      }
+    };
+  }
+
+  function onDiagnosisMenuDoSubtask5c() {
+    return {
+      function: "on_diagnosis_menu_do_subtask_5c_HMD",
+      parameter: {
+        display_string: "Wait for go to proceed from MCC. Say 'Ursa, Next Step' to proceed."
+      }
+    };
+  }
+  
+  function onDiagnosisMenuDoSubtask6() {
+    return {
+      function: "on_diagnosis_menu_do_subtask_6_HMD",
+      parameter: {
+        display_string: "Congratulations, you've completed cable repair!"
+      }
+    };
+  }
+  
+  
+  module.exports = {
+    onDiagnosisMenuBeginSubtask1,
+    onDiagnosisMenuBeginSubtask2,
+    onDiagnosisMenuBeginSubtask3,
+    onDiagnosisMenuBeginSubtask4,
+    onDiagnosisMenuBeginSubtask5,
+    onDiagnosisMenuDoSubtask1a,
+    onDiagnosisMenuDoSubtask1b,
+    onDiagnosisMenuDoSubtask1c,
+    onDiagnosisMenuDoSubtask2a,
+    onDiagnosisMenuDoSubtask2b,
+    onDiagnosisMenuDoSubtask2c,
+    onDiagnosisMenuDoSubtask3a,
+    onDiagnosisMenuDoSubtask3b,
+    onDiagnosisMenuDoSubtask3c,
+    onDiagnosisMenuDoSubtask3d,
+    onDiagnosisMenuDoSubtask3e,
+    onDiagnosisMenuDoSubtask3f,
+    onDiagnosisMenuDoSubtask3g,
+    onDiagnosisMenuDoSubtask4a,
+    onDiagnosisMenuDoSubtask4b,
+    onDiagnosisMenuDoSubtask4c,
+    onDiagnosisMenuDoSubtask4d,
+    onDiagnosisMenuDoSubtask5a,
+    onDiagnosisMenuDoSubtask5b,
+    onDiagnosisMenuDoSubtask5c,
+    onDiagnosisMenuDoSubtask6
+  };  
+  
